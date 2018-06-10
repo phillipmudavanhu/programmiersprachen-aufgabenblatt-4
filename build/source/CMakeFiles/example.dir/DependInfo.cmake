a6e4eece72b987200e879abe84677776045b624f
phillipmudavanhu <philcreativ@gmail.com>
2018-06-10 23:50:05 +0200

    build

---
+++ build/source/CMakeFiles/example.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/phil/Git/Programmierung/programmiersprachen-aufgabenblatt-4/source/glew.c" "/home/phil/Git/Programmierung/programmiersprachen-aufgabenblatt-4/build/source/CMakeFiles/example.dir/glew.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../external"
+  "../external/glfw-3.2.1/include"
+  "../external/nanovg/src"
+  "../external/catch-1.1"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/phil/Git/Programmierung/programmiersprachen-aufgabenblatt-4/source/example.cpp" "/home/phil/Git/Programmierung/programmiersprachen-aufgabenblatt-4/build/source/CMakeFiles/example.dir/example.cpp.o"
+  "/home/phil/Git/Programmierung/programmiersprachen-aufgabenblatt-4/source/window.cpp" "/home/phil/Git/Programmierung/programmiersprachen-aufgabenblatt-4/build/source/CMakeFiles/example.dir/window.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLEW_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external"
+  "../external/glfw-3.2.1/include"
+  "../external/nanovg/src"
+  "../external/catch-1.1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/phil/Git/Programmierung/programmiersprachen-aufgabenblatt-4/build/external/glfw-3.2.1/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/phil/Git/Programmierung/programmiersprachen-aufgabenblatt-4/build/CMakeFiles/nanovg.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
